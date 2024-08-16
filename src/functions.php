855ded4f8fc96db2c7407ec3a7442a2b0586bedc
rsanzfloridauni <rsanz@florida-uni.es>
2024-08-16 18:49:30 +0200

    Añade función despedida en functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,3 +1,4 @@
 <?php function saludar() { return "Hola desde la función!"; } ?>
 <?php function saludarPersonalizado($nombre) { return "Hola, $nombre!"; } ?>
 
+<?php function despedida() { return "Adiós!"; } ?>
